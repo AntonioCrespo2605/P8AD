3f05815a9bfce144cc7cc4b960e02cb58a917f3c
unknown <antoniocrespgome@gmail.com>
2022-12-01 14:46:34 +0100

    .

--- SpoonTieFiveMusic/audioteca.py
+++ SpoonTieFiveMusic/audioteca.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 from tkinter.filedialog import askopenfilenames
 
 #needed variables
-pages=1
+page=0
 
 #xml file
 filename="MusicaXML/audioteca.xml"
@@ -22,6 +22,7 @@ icon_new = PhotoImage(file = "src/anhadirverde2.png")
 icon_change_green=PhotoImage(file = "src/cambiarverde2.png")
 icon_cd=PhotoImage(file = "src/disco.png")
 arrow_icon=PhotoImage(file = "src/flecha.png")
+arrowLeft_icon=PhotoImage(file="src/flechaizq.png")
 
 #Basic Labels and Buttons
 l1=Button(wndw,
@@ -46,9 +47,11 @@ l4=Label(wndw,
         text="fichero: cambiar en codigo",
         font="Verdana 15",
         background="black",
-        fg="#00bf36").grid(row=2, column=1, columnspan=2, sticky="s")
+        fg="#00bf36")
+l4.grid(row=2, column=1, columnspan=2, sticky="s")        
 
 l4_2=Label(
+        wndw,
         text="a",
         font="Verdana 15",
         background="black",
@@ -60,15 +63,18 @@ l5=Label(wndw,
         text="añadir album",
         font="Verdana 15",
         background="black",
-        fg="#949494").grid(row=1, column=0) 
+        fg="#949494")
+l5.grid(row=1, column=0)         
 
 l6=Label(wndw,
         text="cambiar xml",
         font="Verdana 15",
         background="black",
-        fg="#949494").grid(row=1, column=3)               
+        fg="#949494")
+l6.grid(row=1, column=3)                       
 
 l7=Label(
+        wndw,
         text="a",
         fg="black",
         background="black",
@@ -76,92 +82,135 @@ l7=Label(
 ).grid(row=3, column=0, columnspan=4)
 
 #functions
-
+def limitname(disknameaux):#returns a cuted string if it is too long 
+        diskname=""
+        for i in range(9):
+                if i<len(disknameaux):diskname+=disknameaux[i]
+        return diskname        
 
 #root for xml
 tree = ET.parse(filename)
 root=tree.getroot()
 
-#disks interface
-#first disk space:
+#DISKS INTERFACE
 numDisks=len(root)
+list_d=[]
+list_n=[]
+nrow=4
+ncolumn=1
+for i in range(6):
+        d=Button(
+                wndw,
+                image=icon_cd,
+                width=150,
+                height=150
+        )
+        n=Label(
+                wndw,
+                font="Verdana 15"
+        )
+        list_d.append(d)
+        list_n.append(n)        
+        if(numDisks>=i+1):
+                if(i%2==0):
+                        list_d[i].grid(row=nrow, column=ncolumn, sticky="e", padx=15, pady=15)
+                else:
+                        list_d[i].grid(row=nrow, column=ncolumn, sticky="w", padx=15, pady=15)
+                name=limitname(root[i].attrib["nombre"])
+                list_n[i].grid(row=nrow, column=ncolumn, sticky="s")
+                list_n[i].configure(text=name)
 
+                if(i%2==1):
+                        ncolumn=1
+                        nrow+=1
+                else:
+                        ncolumn=2
 
-d1=Button(
-        image=icon_cd,
-        width=150,
-        height=150
-).grid(row=4, column=1, sticky="e", padx=15, pady=10)
+#creating right and left arrows
+arrowR=Button(
+        wndw,
+        image=arrow_icon,
+        width=30,
+        height=30,
+        background="white"
+)
 
-n1=Label(
+arrowL=Button(
+        wndw,
+        image=arrowLeft_icon,
+        width=30,
+        height=30,
+        background="white"
+)
 
-).grid(row=4, column=1, sticky="s")
-#second disk interface:
-d2=Button(
-        image=icon_cd,
-        width=150,
-        height=150
-).grid(row=4, column=2, sticky="w", padx=15, pady=10)
-
-n2=Label(
-
-).grid(row=4, column=2, sticky="s")
-#third disk interface:
-d3=Button(
-        image=icon_cd,
-        width=150,
-        height=150
-).grid(row=5, column=1, sticky="e", padx=15, pady=10)
-
-n3=Label(
+#more functions
+def changeDisksView():
+        global root
+        global page
+        global numDisks
+        global list_d
+        global list_n
+        position=page*6
         
-).grid(row=5, column=1, sticky="s")
-#fourth disk space:
-d4=Button(
-        image=icon_cd,
-        width=150,
-        height=150
-).grid(row=5, column=2, sticky="w", padx=15, pady=10)
+        nrow=4
+        ncolumn=1
+        for i in range(6):
+                if(position+1<=numDisks):
+                        if(i%2==0):
+                                list_d[i].grid(row=nrow, column=ncolumn, sticky="e", padx=15, pady=15)
+                        else:
+                                list_d[i].grid(row=nrow, column=ncolumn, sticky="w", padx=15, pady=15)       
+                        list_n[i].grid(row=nrow, column=ncolumn, sticky="s")
 
-n4=Label(
+                        list_n[i].config(
+                                text=limitname(root[position].attrib["nombre"])
+                        )
+                else:
+                        list_n[i].grid_forget()
+                        list_d[i].grid_forget()
 
-).grid(row=5, column=2, sticky="s")
-#fifth disk interface:
-d5=Button(
-        image=icon_cd,
-        width=150,
-        height=150
-).grid(row=6, column=1, sticky="e", padx=15, pady=10)
+                if(i%2==1):
+                        ncolumn=1
+                        nrow+=1
+                else:
+                        ncolumn=2        
+                position+=1                
 
-n5=Label(
 
-).grid(row=6, column=1, sticky="s")
-#sixth disk interface:
-d6=Button(
-        image=icon_cd,
-        width=150,
-        height=150
-).grid(row=6, column=2, sticky="w", padx=15, pady=10)
+def showDisksRight():
+        global arrowR
+        global arrowL
+        global page
+        page+=1
+        lastDiskOfGroup=(page*6)+5
+        
+        if(numDisks<=lastDiskOfGroup+1):
+                arrowR.grid_forget()
+        arrowL.grid(row=7, column=1)
+        changeDisksView()
 
-n6=Label(
+def showDisksLeft():
+        global arrowR
+        global arrowL
+        global page
+        page-=1
+        if(page==0):
+                arrowL.grid_forget()
+        arrowR.grid(row=7, column=2)
+        changeDisksView()
 
-).grid(row=6, column=2, sticky="s")
-
-#right arrow configuration
-if(len(root)>6):
-        flecha=Button(
-                image=arrow_icon,
-                width=30,
-                height=30,
-                background="white"
-        ).grid(row=7, column=2)
-
+#arrows configuration
+if(numDisks>6):
+        arrowR.config(command=showDisksRight)
+        arrowR.grid(row=7, column=2)
+        arrowL.config(command=showDisksLeft)
         spaceaux=Label(
+                wndw,
                 background="black",
                 text=" ",
                 fg="white",
                 height=10
-        ).grid(row=7, column=1)
+        ).grid(row=7, column=0)
 
 
 
--- SpoonTieFiveMusic/notas.py
+++ SpoonTieFiveMusic/notas.py
@@ -3,132 +3,28 @@ from tkinter import *
 from tkinter import ttk
 from tkinter.filedialog import askopenfilename
 
-cadena="hola"
-print(cadena[5])
+ventana=Tk()
 
-filename = askopenfilename(filetypes=[("XML", ".xml")])
-if(filename==""):
-    filename="MusicaXML/audioteca.xml"
-tree = ET.parse(filename)
 
-audioteca_raiz=tree.getroot()
+cont=0
 
-#MOSTRAR DISCOS
-def mostrar_discos(audioteca_raiz):
-    for child in audioteca_raiz:
-        print(child.attrib["nombre"])
+def hideWidget():
+    global cont
+    print(cont)
+    cont+=1
 
-#MOSTRAR CANCIONES EN FUNCION AL DISCO
-def mostrar_canciones(audioteca_raiz):
-    cont=0
-    for child in audioteca_raiz:
-        print(str(cont)+"-"+child.attrib["nombre"])
-        cont+=1
-    print("escribe pos")
-    pos=int(input())
-    
-    for child in audioteca_raiz[pos]:
-        for subchild in child:
-            print(subchild.attrib["nombre"])
-            
-#BORRAR DISCO
-def borrar_disco(tree, audioteca_raiz, filename):
-    cont=0
-    for child in audioteca_raiz:
-        print(str(cont)+"-"+child.attrib["nombre"])
-        cont+=1
-    print("escribe pos")
-    pos=int(input())
+b=Button(
+    ventana,
+    width=1,
+    height=1,
+    background="black",
+    text="hola",
+    command=hideWidget
+)
 
-    cont=0
-    for child in audioteca_raiz:
-        if(cont==pos):
-            audioteca_raiz.remove(child)
-        cont+=1
-    
-    tree.write(filename)
-
-#MENU
-repeat=True
-while repeat:
-    repeat=True
-
-    print("1-Mostrar discos\n2-Mostar canciones de un disco\n3-borrar disco\n4-salir")
-    op=int(input())
-
-    if(op==1):
-        mostrar_discos(audioteca_raiz)
-    elif(op==2):
-        mostrar_canciones(audioteca_raiz)
-    elif(op==3):
-        borrar_disco(tree, audioteca_raiz, filename)
-    elif(op==4):
-        repeat=False    
-    else:
-        print("opcion desconocida")
+b.grid(column=0, row=0)
 
 
 
-"""
-l8=Button(
-        image=icon_cd,
-        width=150,
-        height=150
-).grid(row=4,column=1, padx=10, pady=10, sticky="e")
-
-l9=Label(
-        text="disco1",
-        font="Verdana 15",
-        fg="black",
-        background="white"
-).grid(row=4,column=1, sticky="s")
-
-l10=Button(
-        image=icon_cd,
-        width=150,
-        height=150
-).grid(row=4,column=2, padx=10, pady=10, sticky="w")
-
-l11=Label(
-        text="disco2",
-        font="Verdana 15",
-        fg="black",
-        background="white"
-).grid(row=4,column=2, sticky="s")
-"""
-  
-"""
-cont=0#the cont is used to know the position of the disk in the root
-for child in root:
-        if(cont<6):
-                c=(cont%2)+1#if the cont is pair the disk must be in the first column. Else it must be in the second
-                
-                if cont%2==0:
-                        r=(cont/2)+4
-                else:
-                        r=((cont-1)/2)+4        
-                disknameaux=child.attrib["nombre"]
-                
-                diskname=""
-                for i in range(9):
-                        if i<len(disknameaux):diskname+=disknameaux[i]
-
-
-                Button(
-                        image=icon_cd,
-                        width=150,
-                        height=150
-                ).grid(row=int(r), column=int(c), padx=10, pady=10, sticky="e")
-
-                Label(
-                        text=diskname,
-                        font="Verdana 15",
-                        fg="black",
-                        background="white"
-                ).grid(row=int(r), column=int(c), sticky="s")
-        
-        cont+=1
-        if(cont>5):
-                break;
-"""
+ventana.mainloop()
 
